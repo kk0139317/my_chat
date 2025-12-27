5123a00d7dc96dc76e2d3ac778663b7d6384ef58
kk0139317 <kk4881757@gmail.com>
2025-12-27 18:52:15 +0530

    first commit

---
+++ convex/schema.ts
@@ -0,0 +1,31 @@
+import { group } from "console"
+import { defineSchema, defineTable } from "convex/server"
+import { v } from "convex/values"
+export default defineSchema({
+  users: defineTable({
+    name: v.optional(v.string()),
+    email: v.string(),
+    image: v.string(),
+    tokenIdentifier: v.string(),
+    isOnline: v.boolean(),
+  }).index("by_tokenIdentifier", ["tokenIdentifier"]),
+
+  conversations: defineTable({
+    participants: v.array(v.id("users")),
+    isGroup: v.boolean(),
+    groupName: v.optional(v.string()),
+    groupImage: v.optional(v.string()),
+    admin: v.optional(v.string()),
+  }),
+
+  messages: defineTable({
+    conversation: v.id("conversations"),
+    sender: v.string(), // ChatGPT string so added as string
+    content: v.string(),
+    messageType: v.union(
+      v.literal("text"),
+      v.literal("image"),
+      v.literal("video")
+    ),
+  }).index("by_conversation", ["conversation"]),
+})
---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "tough-canary-303.convex.cloud" }],
+  },
+}
+
+export default nextConfig
